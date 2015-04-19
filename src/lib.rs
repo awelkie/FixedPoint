7ab79b9be347749dfd2ee0dbf808a97e6967f42e
Allen Welkie <allen@localhost.localdomain>
2015-04-19 14:43:08 -0400

    Made a macro for fixed point trait implications.

--- src/lib.rs
+++ src/lib.rs
@@ -2,83 +2,94 @@ extern crate num;
 
 use std::ops::{Add, Sub, Mul, Div, Rem};
 use std::cmp::{PartialEq, Eq};
-use num::{Zero, One};
+use num::{Zero, One, Num};
 
-struct Fixed24p8 {
-    base: u32,
-}
-
-const NUM_FRAC_BITS: u32 = 8;
-
-impl Add for Fixed24p8 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        Fixed24p8 {
-            base: self.base + rhs.base
+macro_rules! fixed_point_impl {
+    ($name:ident: $ty:ty, $tyd:ty, $fbits:expr) => {
+        struct $name {
+            base: $ty,
         }
-    }
-}
 
-impl Sub for Fixed24p8 {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self {
-        Fixed24p8 {
-            base: self.base - rhs.base
+        impl Add for $name {
+            type Output = Self;
+            fn add(self, rhs: Self) -> Self {
+                $name {
+                    base: self.base + rhs.base
+                }
+            }
         }
-    }
-}
 
-impl Mul for Fixed24p8 {
-    type Output = Self;
-    fn mul(self, rhs: Self) -> Self {
-        Fixed24p8 {
-            base: ((self.base as u64 * rhs.base as u64) >> NUM_FRAC_BITS) as u32,
+        impl Sub for $name {
+            type Output = Self;
+            fn sub(self, rhs: Self) -> Self {
+                $name {
+                    base: self.base - rhs.base
+                }
+            }
         }
-    }
-}
 
-impl Div for Fixed24p8 {
-    type Output = Self;
-    fn div(self, rhs: Self) -> Self {
-        Fixed24p8 {
-            base: (((self.base as u64) << NUM_FRAC_BITS) / (rhs.base as u64)) as u32,
+        impl Mul for $name {
+            type Output = Self;
+            fn mul(self, rhs: Self) -> Self {
+                $name {
+                    base: ((self.base as $tyd * rhs.base as $tyd) >> $fbits) as $ty,
+                }
+            }
         }
-    }
-}
 
-impl Rem for Fixed24p8 {
-    type Output = Self;
-    fn rem(self, rhs: Self) -> Self {
-        Fixed24p8 {
-            base: self.base % rhs.base,
+        impl Div for $name {
+            type Output = Self;
+            fn div(self, rhs: Self) -> Self {
+                $name {
+                    base: (((self.base as $tyd) << $fbits) / (rhs.base as $tyd)) as $ty,
+                }
+            }
         }
-    }
-}
 
-impl Zero for Fixed24p8 {
-    fn zero() -> Self {
-        Fixed24p8 {
-            base: 0,
+        impl Rem for $name {
+            type Output = Self;
+            fn rem(self, rhs: Self) -> Self {
+                $name {
+                    base: self.base % rhs.base,
+                }
+            }
         }
-    }
 
-    fn is_zero(&self) -> bool {
-        self.base == 0
-    }
-}
+        impl Zero for $name {
+            fn zero() -> Self {
+                $name {
+                    base: 0,
+                }
+            }
 
-impl One for Fixed24p8 {
-    fn one() -> Self {
-        Fixed24p8 {
-            base: 1 << NUM_FRAC_BITS,
+            fn is_zero(&self) -> bool {
+                self.base == 0
+            }
         }
-    }
+
+        impl One for $name {
+            fn one() -> Self {
+                $name {
+                    base: 1 << $fbits,
+                }
+            }
+        }
+
+        impl PartialEq for $name {
+            fn eq(&self, other: &Self) -> bool {
+                self.base == other.base
+            }
+        }
+
+        impl Eq for $name { }
+
+        impl Num for $name {
+            type FromStrRadixErr = ();
+            fn from_str_radix(_str: &str, _radix: u32) -> Result<Self, Self::FromStrRadixErr> {
+                unimplemented!();
+            }
+        }
+    };
 }
 
-impl PartialEq for Fixed24p8 {
-    fn eq(&self, other: &Self) -> bool {
-        self.base == other.base
-    }
-}
-
-impl Eq for Fixed24p8 { }
+fixed_point_impl!(Fixed24p8: u32, u64, 8);
